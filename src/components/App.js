81a92236239d7b328222689c4d8e472ffa7decc8
Fernan <fernanhick@gmail.com>
2021-01-22 03:34:58 +0000

    added navbar component

--- src/components/App.js
+++ src/components/App.js
@@ -8,26 +8,33 @@ import Login from './Login';
 import PrivateRoute from './PrivateRoute';
 import forgotPassword from './ForgotPassword';
 import UpdateProfile from './UpdateProfile';
+import NavbarComponent from './Navbar';
 function App() {
   return (
-    <Container
-      className="d-flex align-items-center justify-content-center"
-      style={{minHeight: '100vh'}}
-    >
-      <div className="w-100" style={{maxWidth: '400px'}}>
-        <Router>
-          <AuthProvider>
-            <Switch>
-              <PrivateRoute exact path="/" component={Dashboard} />
-              <PrivateRoute path="/update-profile" component={UpdateProfile} />
-              <Route path="/signup" component={Signup} />
-              <Route path="/login" component={Login} />
-              <Route path="/forgot-password" component={forgotPassword} />
-            </Switch>
-          </AuthProvider>
-        </Router>
-      </div>
-    </Container>
+    <>
+      <NavbarComponent />
+      <Container
+        className="d-flex align-items-center justify-content-center"
+        style={{minHeight: '100vh'}}
+      >
+        <div className="w-100" style={{maxWidth: '400px'}}>
+          <Router>
+            <AuthProvider>
+              <Switch>
+                <PrivateRoute exact path="/" component={Dashboard} />
+                <PrivateRoute
+                  path="/update-profile"
+                  component={UpdateProfile}
+                />
+                <Route path="/signup" component={Signup} />
+                <Route path="/login" component={Login} />
+                <Route path="/forgot-password" component={forgotPassword} />
+              </Switch>
+            </AuthProvider>
+          </Router>
+        </div>
+      </Container>
+    </>
   );
 }
 
